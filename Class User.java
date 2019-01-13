3d617d046084da22f362679f43fea81df55ff5f9
Kumar Gaurav <kumargaurav@Kumars-MacBook-Air.local>
2019-01-13 20:11:11 +0530

    demo file added java

---
+++ Class User.java
@@ -0,0 +1,26 @@
+Class User{
+	
+	piblic User()
+	{
+	////
+	}
+	public static void main(String[] args)
+	{
+			User Obj=new User();
+			String name= obj.getName();
+			s.o.p(name);
+			s.o.p(obj.getName());
+		////
+		if(name.equals("default")){
+		//
+		}
+		else{
+
+		///
+		}
+	}
+	private String getName(){
+		return "default";
+	}
+
+}
